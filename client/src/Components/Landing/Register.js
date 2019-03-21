77f6c764497c33ac876d8a075fac9311135fd330
Daniel Totten <TottenDC@gmail.com>
2019-03-21 11:05:12 -0400

    Finishes Register component

--- client/src/Components/Landing/Register.js
+++ client/src/Components/Landing/Register.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// Icon Imports
+import airplaneIcon from '../../icons/airplane.svg';
+import truckIcon from '../../icons/truck.svg';
+import rocketIcon from '../../icons/truck.svg';
+import earthIcon from '../../icons/earth.svg';
+
 class Register extends Component {
     
     state = {
         email: '',
         password: '',
         confirmPassword: '',
-        icon: '',
+        icon: 'airplane',
     }
 
     handleInputChange = (event) => {
@@ -31,7 +37,7 @@ class Register extends Component {
     render() {
         return (
             <div className="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
-                <div className="modal-dialog" role="document">
+                <div className="modal-dialog modal-dialog-centered" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="registerModalLabel">Register</h5>
@@ -40,7 +46,80 @@ class Register extends Component {
                             </button>
                         </div>
                         <div className="modal-body">
-                            [// todo Finish the form]
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label for="email">Email address</label>
+                                    <input type="email" className="form-control" id="email" name="email" value={this.state.email} onChange={this.handleInputChange} aria-describedby="emailHelp" placeholder="example@email.com" required />
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div className="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" className="form-control" id="password" name="password" value={this.state.password} onChange={this.handleInputChange} placeholder="Password" required />
+                                </div>
+                                <div className="form-group">
+                                    <label for="confirmPassword">Confirm password</label>
+                                    <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} placeholder="Please confirm your password" required />
+                                </div>
+                                <div className="form-group">
+                                    <div className="form-check form-check-inline">
+                                        <input 
+                                            className="form-check-input" 
+                                            type="radio" 
+                                            name="icon"
+                                            id='radio1' 
+                                            value={airplaneIcon} 
+                                            checked={this.state.icon === airplaneIcon} 
+                                            onChange={this.handleInputChange} 
+                                        />
+                                        <label className="form-check-label" for="radio1">
+                                            <img className="img-fluid" src={airplaneIcon} alt='airplane' />
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input 
+                                            className="form-check-input" 
+                                            type="radio" 
+                                            name="icon"
+                                            id='radio2' 
+                                            value={truckIcon}
+                                            checked={this.state.icon === truckIcon} 
+                                            onChange={this.handleInputChange} 
+                                        />
+                                        <label className="form-check-label" for="radio2">
+                                            <img className='img-fluid' src={truckIcon} alt='truck' />
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input 
+                                            className="form-check-input" 
+                                            type="radio" 
+                                            name="icon"
+                                            id='radio3' 
+                                            value={rocketIcon} 
+                                            checked={this.state.icon === rocketIcon} 
+                                            onChange={this.handleInputChange} 
+                                        />
+                                        <label className="form-check-label" for="radio3">
+                                            <img className="img-fluid" src={rocketIcon} alt="rocket" />
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input 
+                                            className="form-check-input" 
+                                            type="radio" 
+                                            name="icon"
+                                            id='radio4' 
+                                            value={earthIcon} 
+                                            checked={this.state.icon === earthIcon} 
+                                            onChange={this.handleInputChange} 
+                                        />
+                                        <label className="form-check-label" for="radio4">
+                                            <img className="img-fluid" src={earthIcon} alt="earth" />
+                                        </label>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
                         </div>
                     </div>
                 </div>
