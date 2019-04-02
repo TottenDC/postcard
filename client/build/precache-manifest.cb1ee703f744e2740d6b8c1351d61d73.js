871c565e66f3429474a5f509904326c322a53c59
Daniel Totten <TottenDC@gmail.com>
2019-04-02 08:50:05 -0400

    Took out the /build from .gitignore

---
+++ client/build/precache-manifest.cb1ee703f744e2740d6b8c1351d61d73.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "9203a94b7f63d2fa8806d25aef7bfe36",
+    "url": "/static/media/stampedBorder.9203a94b.svg"
+  },
+  {
+    "revision": "16294c3ef65d887e6a51",
+    "url": "/static/css/main.f61f7794.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a5c5cac951a569a4c9",
+    "url": "/static/js/2.c7b2a7a4.chunk.js"
+  },
+  {
+    "revision": "000b44a24b2f46f6a8ea9d099e3e7413",
+    "url": "/static/media/airplane.000b44a2.svg"
+  },
+  {
+    "revision": "367870a74ee54bc2b3d9e59820a962db",
+    "url": "/static/media/error.367870a7.svg"
+  },
+  {
+    "revision": "2f61aa88ee79e1dc468c02b0860edc9a",
+    "url": "/static/media/truck.2f61aa88.svg"
+  },
+  {
+    "revision": "b28d223c3cfa797354cb8ba3c8c1b583",
+    "url": "/static/media/rocket.b28d223c.svg"
+  },
+  {
+    "revision": "43645cafa4e07f4b8f68f6610d09d61d",
+    "url": "/static/media/earth.43645caf.svg"
+  },
+  {
+    "revision": "084504c9bbf5dca0cef952eb57c6486c",
+    "url": "/static/media/postmark.084504c9.svg"
+  },
+  {
+    "revision": "9ea57640dddc27ac8e1925e9f9ae27a7",
+    "url": "/static/media/Yelp_trademark_RGB.9ea57640.png"
+  },
+  {
+    "revision": "8420887167a9b4b4412fe4d1dddd4985",
+    "url": "/static/media/mainborder.84208871.svg"
+  },
+  {
+    "revision": "757ad3ecdaba299e76b8d1ec1e395e98",
+    "url": "/static/media/stampOutline.757ad3ec.svg"
+  },
+  {
+    "revision": "16294c3ef65d887e6a51",
+    "url": "/static/js/main.6d9b7dbe.chunk.js"
+  },
+  {
+    "revision": "bfb970aa51485f14f7fb0ddd93291f8c",
+    "url": "/static/media/paper_1280.bfb970aa.jpg"
+  },
+  {
+    "revision": "a7770cab1b25b3a639c45cd06cb75e13",
+    "url": "/static/media/overcast.a7770cab.svg"
+  },
+  {
+    "revision": "223db1b29828d30030755e83a78b6808",
+    "url": "/static/media/clearDay.223db1b2.svg"
+  },
+  {
+    "revision": "2d144f9850a61724916cc089d22d3a8a",
+    "url": "/static/media/clearNight.2d144f98.svg"
+  },
+  {
+    "revision": "3b9cc44d137ec34970809c7f9230d46f",
+    "url": "/static/media/cloudyDay.3b9cc44d.svg"
+  },
+  {
+    "revision": "c860321dc36b6289d67d9d01b9c8505d",
+    "url": "/static/media/cloudyNight.c860321d.svg"
+  },
+  {
+    "revision": "0e9c010c531008c0f5304c98eae9867d",
+    "url": "/static/media/fog.0e9c010c.svg"
+  },
+  {
+    "revision": "4aa86aa85d0be7d214847121d929eb55",
+    "url": "/static/media/rain.4aa86aa8.svg"
+  },
+  {
+    "revision": "d52291c983e9ffb8710f24036430ac39",
+    "url": "/static/media/snow.d52291c9.svg"
+  },
+  {
+    "revision": "e3768d18c18843ded88e3b86601a1d71",
+    "url": "/static/media/wind.e3768d18.svg"
+  },
+  {
+    "revision": "1e51be77d12f72ea9371c0602e2fd6f4",
+    "url": "/static/media/error.1e51be77.svg"
+  },
+  {
+    "revision": "07fe70cffe8f5b940458f65cb65da6f6",
+    "url": "/index.html"
+  }
+];
