ac77012bffedd06310f3f476a0c51f5c96f53280
Daniel Totten <TottenDC@gmail.com>
2019-04-02 08:06:16 -0400

    Fixes modal issue for 404 and error pages.

--- client/src/Components/Error/Errors.js
+++ client/src/Components/Error/Errors.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import errIcon from '../../icons/appIcons/error.svg';
 
-const Errors = () => {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col">
-                    <img className="img-fluid mx-auto d-block" src={errIcon} alt="Error icon" />
+class Errors extends Component {
+
+    componentDidMount() {
+        let docBody = document.querySelector('body');
+        if (docBody.getAttribute('class') === 'modal-open') {
+            docBody.setAttribute('class', '');
+            const modalHideDiv = document.querySelector('.modal-backdrop');
+            modalHideDiv.style.display = 'none';
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col">
+                        <img className="img-fluid mx-auto d-block" src={errIcon} alt="Error icon" />
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-5">
+                    <div className="col text-center">
+                        <h1>Oh no!</h1>
+                        <br />
+                        <p className="largestText">There appears to have been an error. Please return home and try again.</p>
+                        <button type="button" className="btn btn-primary btn-logout">
+                            <a href="/" className="text-reset">Return home</a>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div className="row justify-content-center mt-5">
-                <div className="col text-center">
-                    <h1>Oh no!</h1>
-                    <br />
-                    <p className="largestText">There appears to have been an error. Please return home and try again.</p>
-                    <button type="button" className="btn btn-primary btn-logout">
-                        <a href="/" className="text-reset">Return home</a>
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Errors;
